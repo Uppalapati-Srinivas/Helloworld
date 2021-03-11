da0030810a9f39a42e9ebf3d889a99b7203fc242
Uppalapati-Srinivas <80322445+Uppalapati-Srinivas@users.noreply.github.com>
2021-03-11 17:01:36 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello {
   public static void main(String[] args) {
-    System.out.println("Hey ! There You are now jenkins");
+    System.out.println("Hey There ! Congo You have completed task1 on jenkins");
   }
 }
